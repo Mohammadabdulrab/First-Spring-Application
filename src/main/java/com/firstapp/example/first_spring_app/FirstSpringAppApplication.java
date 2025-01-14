b9508d020fd0e4e4cfb5ac8f2d0d1241c773412f
Mohammadabdulrab <mohammadabdulrab78760@gmail.com>
2025-01-14 16:48:54 +0530

    Add even/odd code in spring boot

--- src/main/java/com/firstapp/example/first_spring_app/FirstSpringAppApplication.java
+++ src/main/java/com/firstapp/example/first_spring_app/FirstSpringAppApplication.java
@@ -9,6 +9,13 @@ public class FirstSpringAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FirstSpringAppApplication.class, args);
 		System.out.println("This is my first spring boot code");
+		int a=10;
+		if(a%2==0){
+			System.out.println(a+" is an even number.");
+		}
+		else{
+			System.out.println(a+" is an odd number");
+		}
 	}
 
 }
